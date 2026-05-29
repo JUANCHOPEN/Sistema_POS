58dc57e0b8d9caeb23d0ed5e0e87932717f704a3
wvasquezn-maker <wvasquezn@miumg.edu.gt>
2026-05-29 13:20:13 -0600

    Update auth.py

--- backend/services/auth.py
+++ backend/services/auth.py
@@ -13,7 +13,7 @@ load_dotenv()
 # Configuración
 SECRET_KEY              = os.getenv("SECRET_KEY")
 ALGORITHM               = os.getenv("ALGORITHM")
-TOKEN_EXPIRE_MINUTES    = int(os.getenv("TOKEN_EXPIRE_MINUTES"))
+TOKEN_EXPIRE_MINUTES = int(os.getenv("TOKEN_EXPIRE_MINUTES") or 60)
 MAX_INTENTOS_FALLIDOS   = 5
 
 # Configuración de bcrypt
